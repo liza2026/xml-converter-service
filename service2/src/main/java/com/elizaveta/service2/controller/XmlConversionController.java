24a5b1ca4ba5b05b354a8e1bfde6351128c04648
Liza Guseva <gusevaliza210@gmail.com>
2026-06-10 22:02:07 +0300

    feat(service2): add XmlConversionController

---
+++ service2/src/main/java/com/elizaveta/service2/controller/XmlConversionController.java
@@ -0,0 +1,45 @@
+package com.elizaveta.service2.controller;
+
+import com.elizaveta.service2.dto.XmlConversionResponse;
+import com.elizaveta.service2.service.XmlConversionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class XmlConversionController {
+
+    private final XmlConversionService conversionService;
+
+    @PostMapping(
+            value = "/xml2format",
+            consumes = MediaType.TEXT_XML_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<XmlConversionResponse> convertXml(@RequestBody String xml) {
+
+        log.info("Получен запрос на конвертацию из XML в JSON");
+        log.debug("Тело запроса: {}", xml);
+
+        try {
+            Object result = conversionService.convert(xml);
+
+            XmlConversionResponse response = new XmlConversionResponse(result);
+
+            log.info("Запрос успешно обработан, возвращаем ответ");
+
+            return ResponseEntity.ok(response);
+
+        } catch (RuntimeException e) {
+            log.error("Ошибка обработки запроса: {}", e.getMessage());
+
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
