c215c402fce8d5ca597ad61c927fd122795e2425
Liza Guseva <gusevaliza210@gmail.com>
2026-06-20 18:34:43 +0300

    feat(service1): configure HikariCP connection pool for Hibernate

--- service1/src/main/java/com/elizaveta/service1/config/HibernateConfig.java
+++ service1/src/main/java/com/elizaveta/service1/config/HibernateConfig.java
@@ -1,9 +1,13 @@
 package com.elizaveta.service1.config;
 
 import com.elizaveta.service1.entity.ConversionRequest;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @org.springframework.context.annotation.Configuration
 public class HibernateConfig {
@@ -27,18 +31,38 @@ public class HibernateConfig {
     private String formatSql;
 
     @Bean
-    public SessionFactory sessionFactory() {
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+
+        hikariConfig.setPoolName("service1-hikari-pool");
+        hikariConfig.setMaximumPoolSize(10);
+        hikariConfig.setMinimumIdle(2);
+        hikariConfig.setConnectionTimeout(30000);
+        hikariConfig.setIdleTimeout(600000);
+        hikariConfig.setMaxLifetime(1800000);
+
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    public SessionFactory sessionFactory(DataSource dataSource) {
         org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
 
-        configuration.setProperty("hibernate.connection.url", url);
-        configuration.setProperty("hibernate.connection.username", username);
-        configuration.setProperty("hibernate.connection.password", password);
-        configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+        configuration.getProperties().put("hibernate.connection.datasource", dataSource);
 
         configuration.setProperty("hibernate.dialect", dialect);
         configuration.setProperty("hibernate.show_sql", showSql);
         configuration.setProperty("hibernate.format_sql", formatSql);
         configuration.setProperty("hibernate.hbm2ddl.auto", "none");
+        configuration.setProperty(
+                "hibernate.current_session_context_class",
+                "org.springframework.orm.jpa.hibernate.SpringSessionContext"
+        );
 
         configuration.addAnnotatedClass(ConversionRequest.class);
 
